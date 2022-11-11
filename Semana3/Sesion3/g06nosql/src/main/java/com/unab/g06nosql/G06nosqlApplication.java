ac9817451526c3f4a3537935a3c13c8a08363568
Jesús Ariel González Bonilla <ariel5253@misena.edu.co>
2022-11-11 08:15:49 -0500

    Sesion 3 Semana 3

---
+++ Semana3/Sesion3/g06nosql/src/main/java/com/unab/g06nosql/G06nosqlApplication.java
@@ -0,0 +1,13 @@
+package com.unab.g06nosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G06nosqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G06nosqlApplication.class, args);
+	}
+
+}
---
+++ Semana3/Sesion3/g06nosql/src/test/java/com/unab/g06nosql/G06nosqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unab.g06nosql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G06nosqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
